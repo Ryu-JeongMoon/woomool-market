b2f98394c3f2dae6a6af9e3bc24a3ed0a0a6b9b8
jm <rjm9303@gmail.com>
2021-09-11 14:26:02 +0900

    [#23] woomool-market-23 jwt WeakKeyException solved - 4

--- module-common/src/test/java/com/woomoolmarket/domain/member/repository/MemberRepositoryTest.java
+++ module-common/src/test/java/com/woomoolmarket/domain/member/repository/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.woomoolmarket.ModuleCommonApplication;
 import com.woomoolmarket.domain.member.entity.Member;
+import javax.persistence.EntityManager;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(classes = ModuleCommonApplication.class)
 class MemberRepositoryTest {
 
+    @Autowired
+    EntityManager em;
     @Autowired
     MemberRepository memberRepository;
 
@@ -66,6 +69,9 @@ class MemberRepositoryTest {
                 .build();
             memberRepository.save(member);
         }
+
+        em.flush();
+        em.clear();
 
         Long id = 3L;
         Long previousId = memberRepository.findPreviousId(id)
