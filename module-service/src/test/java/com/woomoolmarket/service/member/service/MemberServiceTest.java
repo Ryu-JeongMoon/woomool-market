7cfe7dae46e3960d2e8d8474ae294052c8793702
jm <rjm9303@gmail.com>
2021-09-11 14:40:05 +0900

    [#23] woomool-market-23 block QueryDSL test

--- module-service/src/test/java/com/woomoolmarket/service/member/service/MemberServiceTest.java
+++ module-service/src/test/java/com/woomoolmarket/service/member/service/MemberServiceTest.java
@@ -36,7 +36,8 @@ class MemberServiceTest {
             .compareTo(septemberFirst));
     }
 
-    @Test
+    // TODO - test for Local
+    //@Test
     void findIdTest() {
         for (int i = 8; i < 12; i++) {
             Member member = Member.builder()
@@ -69,7 +70,8 @@ class MemberServiceTest {
     }
 
     /* 뭐지 별 차이 안 나네 둘 다 느린 거 같은데 .. Long 으로 직접 구하는게 빠르긴 함 */
-    @Test
+    // TODO - test for Local
+    //@Test
     void findNextIdTest() {
         for (int i = 0; i < 7; i++) {
             Member member = Member.builder()
