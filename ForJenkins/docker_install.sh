9973343cff29aacd23442c7f1cbbb64e15f36eb8
jm <rjm9303@gmail.com>
2021-09-12 02:27:04 +0900

    [#26] woomool-market-26 Jenkins CI / CD 도입

---
+++ ForJenkins/docker_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+apt-get update && \
+apt-get -y install apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg2 \
+  zip \
+  unzip \
+  software-properties-common && \
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey && \
+add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+$(lsb_release -cs) \
+stable" && \
+apt-get update && \
+apt-get -y install docker-ce && \
+apt-get -y install nodejs && \
+apt-get -y install npm
