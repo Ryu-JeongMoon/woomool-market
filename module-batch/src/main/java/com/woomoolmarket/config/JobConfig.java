73ccb29f177fd4e5a201f239d64e752d62cc831b
jm <rjm9303@gmail.com>
2021-09-12 14:47:54 +0900

    [#4] schema-mysql.sql을 통한 Local DB에 schema 생성

--- module-batch/src/main/java/com/woomoolmarket/config/JobConfig.java
+++ module-batch/src/main/java/com/woomoolmarket/config/JobConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,15 +23,17 @@ public class JobConfig {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep())
+            .start(simpleStep(null))
             .build();
     }
 
     @Bean
-    public Step simpleStep() {
+    @JobScope
+    public Step simpleStep(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep")
             .tasklet(((contribution, chunkContext) -> {
                 log.info(">>> step 1");
+                log.info("requestDate = {}", requestDate);
                 return RepeatStatus.FINISHED;
             }))
             .build();
