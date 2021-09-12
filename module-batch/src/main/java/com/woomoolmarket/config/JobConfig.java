4798fb4067319f33e07eac8d5de851b2542d09b7
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2021-09-12 21:36:02 +0900

    [#4] Introduction to Spring Batch Job & Step
    * [#4] schema-mysql.sql을 통한 Local DB에 schema 생성
    
    * [#4] job & step 의 이해

--- module-batch/src/main/java/com/woomoolmarket/config/JobConfig.java
+++ module-batch/src/main/java/com/woomoolmarket/config/JobConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,15 +23,30 @@ public class JobConfig {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep())
+            .start(simpleStep1(null))
+            .next(simpleStep2(null))
             .build();
     }
 
     @Bean
-    public Step simpleStep() {
-        return stepBuilderFactory.get("simpleStep")
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep1")
             .tasklet(((contribution, chunkContext) -> {
                 log.info(">>> step 1");
+                log.info("requestDate = {}", requestDate);
+                return RepeatStatus.FINISHED;
+            }))
+            .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+            .tasklet(((contribution, chunkContext) -> {
+                log.info(">>> step 2");
+                log.info("requestDate = {}", requestDate);
                 return RepeatStatus.FINISHED;
             }))
             .build();
