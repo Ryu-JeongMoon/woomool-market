8120b157382ccb08b9d9503ead0276acfbb2e6e8
jm <rjm9303@gmail.com>
2021-09-12 14:52:35 +0900

    [#31] woomool-market-31 RefreshToken data 변경

--- module-common/src/main/java/com/woomoolmarket/domain/token/repository/RefreshTokenRepository.java
+++ module-common/src/main/java/com/woomoolmarket/domain/token/repository/RefreshTokenRepository.java
@@ -1,6 +1,6 @@
 package com.woomoolmarket.domain.token.repository;
 
-import com.woomoolmarket.domain.token.RefreshToken;
+import com.woomoolmarket.domain.token.entiy.RefreshToken;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
