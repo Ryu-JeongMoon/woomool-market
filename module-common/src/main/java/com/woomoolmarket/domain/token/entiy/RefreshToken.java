0b4a58899cc32a2ba31e1df6d0b8f4a70f708192
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2021-09-12 14:53:04 +0900

    [#31] woomool-market-31 RefreshToken data 변경

--- module-common/src/main/java/com/woomoolmarket/domain/token/entiy/RefreshToken.java
+++ module-common/src/main/java/com/woomoolmarket/domain/token/entiy/RefreshToken.java
@@ -1,8 +1,10 @@
-package com.woomoolmarket.domain.token;
+package com.woomoolmarket.domain.token.entiy;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,11 @@ import lombok.NoArgsConstructor;
 public class RefreshToken {
 
     @Id
+    @Column(name = "token_key")
     private String key;
+
+    @Lob
+    @Column(name = "token_value")
     private String value;
 
     @Builder
