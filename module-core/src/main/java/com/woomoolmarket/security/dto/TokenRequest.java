ee4f8dc140614a021b49b79eb806b640e92fbce8
jm <rjm9303@gmail.com>
2021-09-17 09:15:30 +0900

    [#46] module-core 중 @SpringBootTest -> @DataJpaTest 로 변경 가능한 것들 정리

--- module-core/src/main/java/com/woomoolmarket/security/dto/TokenRequest.java
+++ module-core/src/main/java/com/woomoolmarket/security/dto/TokenRequest.java
@@ -1,10 +1,12 @@
 package com.woomoolmarket.security.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TokenRequest {
 
     private String accessToken;
