408d3b981becd2b74255d5b1dbd2f649cea339db
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2021-09-25 23:35:12 +0900

    [#73] 아이디 중복 시 중복 예외 발생하도록 로직 추가
    * [#73] Exception Log 남기기 위한 AOP annotation 추가
    
    * [#73] 아이디 중복 시 중복 예외 발생하도록 로직 추가

--- module-core/src/main/java/com/woomoolmarket/common/util/ExceptionUtil.java
+++ module-core/src/main/java/com/woomoolmarket/common/util/ExceptionUtil.java
@@ -3,6 +3,8 @@ package com.woomoolmarket.common.util;
 public class ExceptionUtil {
 
     public static final String USER_NOT_FOUND = "존재하지 않는 회원입니다";
+    public static final String DUPLICATED_USER = "이미 존재하는 회원 아이디입니다";
+
     public static final String CANNOT_CANCEL = "배송이 완료된 상품은 취소할 수 없습니다";
     public static final String NOT_ENOUGH_STOCK = "재고를 초과해 주문할 수 없습니다";
     public static final String PRODUCT_NOT_FOUND = "존재하지 않는 상품입니다";
