dc8439defc9a4209c7e4e0f14ec62e5a0890f1fb
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2021-09-26 01:02:52 +0900

    [#37] Exception 발생 시, GlobalExceptionHandler 통해 반환 코드와 ExceptionResponse 반환 (#77)

--- module-api/src/main/java/com/woomoolmarket/exception/handler/GlobalExceptionHandler.java
+++ module-api/src/main/java/com/woomoolmarket/exception/handler/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.woomoolmarket.exception.handler;
 
 import com.woomoolmarket.aop.exception.LogForException;
+import com.woomoolmarket.errors.ExceptionResponse;
 import com.woomoolmarket.exception.member.UsernameDuplicatedException;
 import com.woomoolmarket.exception.product.NotEnoughStockException;
 import com.woomoolmarket.exception.product.ProductNameNotFoundException;
-import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,37 +18,32 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static String getExceptionName(Exception e) {
+    private static String getExceptionClass(Exception e) {
         return e != null ? e.getClass().getSimpleName() : "";
     }
 
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity handleIllegalArgumentException(Exception e) {
-        return ResponseEntity.of(Optional.of(e.getMessage() + getExceptionName(e)));
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(ExceptionResponse.of(getExceptionClass(e), e.getMessage()));
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = UsernameNotFoundException.class)
     public ResponseEntity handleNotFoundException(Exception e) {
-        return ResponseEntity.of(Optional.of(e.getMessage() + getExceptionName(e)));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.of(getExceptionClass(e), e.getMessage()));
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = UsernameDuplicatedException.class)
     public ResponseEntity handleUsernameDuplicatedException(Exception e) {
-        return ResponseEntity.of(Optional.of(e.getMessage() + getExceptionName(e)));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.of(getExceptionClass(e), e.getMessage()));
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = ProductNameNotFoundException.class)
     public ResponseEntity handleProductNameNotFoundException(Exception e) {
-        return ResponseEntity.of(Optional.of(e.getMessage() + getExceptionName(e)));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.of(getExceptionClass(e), e.getMessage()));
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = NotEnoughStockException.class)
     public ResponseEntity handleNotEnoughStockException(Exception e) {
-        return ResponseEntity.of(Optional.of(e.getMessage() + getExceptionName(e)));
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(ExceptionResponse.of(getExceptionClass(e), e.getMessage()));
     }
 }
