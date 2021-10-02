d70a698f4b92ea0cdfb974b3fc41999963e54e11
jm <rjm9303@gmail.com>
2021-10-02 21:56:51 +0900

    [#63] Ready for Asynchronous Config, Service

---
+++ module-core/src/main/java/com/woomoolmarket/config/async/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.woomoolmarket.config.async;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(4);
+        executor.setThreadNamePrefix("woomool-market async-");
+        executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ module-service/src/main/java/com/woomoolmarket/service/async/AsyncService.java
@@ -0,0 +1,9 @@
+package com.woomoolmarket.service.async;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsyncService {
+
+
+}
