e0fbf6c2bdfea748d411d034d54e111b32b31669
jm <rjm9303@gmail.com>
2021-10-04 01:25:05 +0900

    [#92] Board domain Service & Controller 수정

--- module-api/src/main/java/com/woomoolmarket/controller/board/BoardController.java
+++ module-api/src/main/java/com/woomoolmarket/controller/board/BoardController.java
@@ -1,87 +1,125 @@
 package com.woomoolmarket.controller.board;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woomoolmarket.aop.time.LogExecutionTime;
-import com.woomoolmarket.controller.board.model.BoardModel;
+import com.woomoolmarket.common.enumeration.Status;
+import com.woomoolmarket.domain.board.repository.BoardSearchCondition;
 import com.woomoolmarket.service.board.BoardService;
-import com.woomoolmarket.service.board.dto.request.RegisterBoardRequest;
+import com.woomoolmarket.service.board.dto.request.ModifyBoardRequest;
+import com.woomoolmarket.service.board.dto.request.BoardRequest;
 import com.woomoolmarket.service.board.dto.response.BoardResponse;
-import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @LogExecutionTime
-@RequestMapping(path = "/api/boards",
-    produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/boards", produces = MediaTypes.HAL_JSON_VALUE)
 public class BoardController {
 
     private final BoardService boardService;
     private final ObjectMapper objectMapper;
+    private final PagedResourcesAssembler<BoardResponse> assembler;
 
     @GetMapping
-    public ResponseEntity getBoards() {
-        return ResponseEntity.ok(boardService.findActiveBoards());
+    public ResponseEntity<PagedModel<EntityModel<BoardResponse>>> getListBySearchConditionForMember(
+        BoardSearchCondition condition, @PageableDefault Pageable pageable) {
+
+        List<BoardResponse> boardResponses = boardService.getListBySearchCondition(condition);
+        PageImpl<BoardResponse> responsePage = new PageImpl<>(boardResponses, pageable, boardResponses.size());
+        return ResponseEntity.ok(assembler.toModel(responsePage));
     }
 
     @PostMapping
-    public ResponseEntity registerBoard(@Validated RegisterBoardRequest registerBoardRequest, BindingResult bindingResult)
+    public ResponseEntity registerBoard(@Validated BoardRequest registerBoardRequest, BindingResult bindingResult)
         throws JsonProcessingException {
-
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(objectMapper.writeValueAsString(bindingResult));
         }
 
-        BoardResponse boardResponse = boardService.registerBoard(registerBoardRequest);
-
-        WebMvcLinkBuilder builder = linkTo(BoardController.class).slash(boardResponse.getId());
-        URI createdUri = builder.toUri();
-        BoardModel boardModel = new BoardModel(boardResponse);
-
-        return ResponseEntity.created(createdUri)
-            .body(boardModel);
+        boardService.register(registerBoardRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getBoard(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<BoardResponse>> getActiveBoardById(@PathVariable Long id) {
         boardService.increaseHit(id);
-        BoardResponse boardResponse = boardService.findActiveBoard(id);
+        BoardResponse boardResponse = boardService.getByIdAndStatus(id, Status.ACTIVE);
+        WebMvcLinkBuilder defaultLink = linkTo(methodOn(BoardController.class).getActiveBoardById(id));
 
-        return ResponseEntity.ok(boardResponse);
+        EntityModel<BoardResponse> boardModel =
+            EntityModel.of(
+                boardResponse,
+                defaultLink.withSelfRel(),
+                defaultLink.withRel("modify-product"),
+                defaultLink.withRel("delete-product"));
+
+        return ResponseEntity.ok(boardModel);
     }
 
-//    @GetMapping("/{nickname}")
-//    public ResponseEntity getBoardByNickname(@PathVariable String nickname) {
-//        return ResponseEntity.ok(boardService.findActiveBoarByNickname(nickname));
-//    }
+    @PatchMapping("/{id}")
+    public ResponseEntity editBoardInfo(@PathVariable Long id,
+        @Validated @RequestBody ModifyBoardRequest modifyRequest, BindingResult bindingResult) throws JsonProcessingException {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(objectMapper.writeValueAsString(bindingResult));
+        }
+
+        BoardResponse boardResponse = boardService.edit(id, modifyRequest);
+        EntityModel<BoardResponse> boardModel =
+            EntityModel.of(boardResponse, linkTo(methodOn(BoardController.class).getActiveBoardById(id)).withSelfRel());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(boardModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBoard(@PathVariable Long id) {
+        boardService.deleteSoftly(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/deleted/{id}")
+    public ResponseEntity<Void> restoreBoard(@PathVariable Long id) {
+        boardService.restore(id);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
 
     /* FOR ADMIN */
-    @GetMapping("/admin/all")
-    public ResponseEntity getAllBoards() {
-        return ResponseEntity.ok(boardService.findAllBoards());
-    }
+    @GetMapping("/admin")
+    public ResponseEntity<PagedModel<EntityModel<BoardResponse>>> getListBySearchConditionForAdmin(
+        BoardSearchCondition condition, @PageableDefault Pageable pageable) {
 
-    @GetMapping("/admin/active")
-    public ResponseEntity getAllActiveBoards() {
-        return ResponseEntity.ok(boardService.findAllActiveBoards());
-    }
-
-    @GetMapping("/admin/inactive")
-    public ResponseEntity getAllInactiveBoards() {
-        return ResponseEntity.ok(boardService.findAllInactiveBoards());
+        List<BoardResponse> boardResponses = boardService.getListBySearchConditionForAdmin(condition);
+        PageImpl<BoardResponse> responsePage = new PageImpl<>(boardResponses, pageable, boardResponses.size());
+        return ResponseEntity.ok(assembler.toModel(responsePage));
     }
 }
+
+/*
+게시글 복구의 경우 URI 어떻게 받을 것인지?
+api/boards/deleted/{id} ??
+ */
---
+++ module-service/src/test/java/com/woomoolmarket/service/board/BoardServiceTest.java
@@ -0,0 +1,189 @@
+package com.woomoolmarket.service.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.woomoolmarket.common.enumeration.Status;
+import com.woomoolmarket.domain.board.entity.Board;
+import com.woomoolmarket.domain.board.entity.BoardCategory;
+import com.woomoolmarket.domain.board.repository.BoardRepository;
+import com.woomoolmarket.domain.board.repository.BoardSearchCondition;
+import com.woomoolmarket.domain.member.entity.Member;
+import com.woomoolmarket.domain.member.repository.MemberRepository;
+import com.woomoolmarket.service.board.dto.request.BoardRequest;
+import com.woomoolmarket.service.board.dto.request.ModifyBoardRequest;
+import com.woomoolmarket.service.board.dto.response.BoardResponse;
+import com.woomoolmarket.service.board.mapper.BoardResponseMapper;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@Transactional
+@SpringBootTest
+class BoardServiceTest {
+
+    private static final String MEMBER_1_EMAIL = "panda@naver.com";
+    private static final String MEMBER_2_EMAIL = "tiger@naver.com";
+    private static final String BOARD_1_TITLE = "panda1";
+    private static final String BOARD_1_CONTENT = "bear1";
+    private static final String BOARD_2_TITLE = "panda2";
+    private static final String BOARD_2_CONTENT = "bear2";
+    private static final String BOARD_3_TITLE = "panda3";
+    private static final String BOARD_3_CONTENT = "bear3";
+
+    @Autowired
+    BoardRepository boardRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    BoardService boardService;
+    @Autowired
+    BoardResponseMapper boardResponseMapper;
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    void init() {
+        boardRepository.deleteAll();
+        em.createNativeQuery("ALTER TABLE BOARD ALTER COLUMN `board_id` RESTART WITH 1").executeUpdate();
+
+        Member member1 = Member.builder()
+            .email(MEMBER_1_EMAIL)
+            .nickname("bear")
+            .password("123456")
+            .build();
+
+        Member member2 = Member.builder()
+            .email(MEMBER_2_EMAIL)
+            .nickname("cat")
+            .password("123456")
+            .build();
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        em.flush();
+
+        BoardRequest board1 = BoardRequest.builder()
+            .member(member1)
+            .title(BOARD_1_TITLE)
+            .content(BOARD_1_CONTENT)
+            .boardCategory(BoardCategory.QNA)
+            .build();
+
+        BoardRequest board2 = BoardRequest.builder()
+            .member(member1)
+            .title(BOARD_2_TITLE)
+            .content(BOARD_2_CONTENT)
+            .boardCategory(BoardCategory.FREE)
+            .build();
+
+        BoardRequest board3 = BoardRequest.builder()
+            .member(member2)
+            .title(BOARD_3_TITLE)
+            .content(BOARD_3_CONTENT)
+            .boardCategory(BoardCategory.NOTICE)
+            .build();
+
+        boardService.register(board1);
+        boardService.register(board2);
+        boardService.register(board3);
+    }
+
+    @Test
+    @DisplayName("검색 조건 생성하지 않을 시 전체 검색")
+    void getListBySearchCondition() {
+        BoardSearchCondition boardSearchCondition = new BoardSearchCondition();
+        List<BoardResponse> boardResponses = boardService.getListBySearchCondition(boardSearchCondition);
+        assertThat(boardResponses.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Admin 전용 검색")
+    void getListBySearchConditionForAdmin() {
+        BoardSearchCondition searchCondition1 = new BoardSearchCondition();
+        List<BoardResponse> boardResponses1 = boardService.getListBySearchConditionForAdmin(searchCondition1);
+
+        BoardSearchCondition searchCondition2 = BoardSearchCondition.builder().status(Status.INACTIVE).build();
+        List<BoardResponse> boardResponses2 = boardService.getListBySearchConditionForAdmin(searchCondition2);
+
+        assertThat(boardResponses1.size()).isEqualTo(3);
+        assertThat(boardResponses2.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("fetch join 결과")
+    void getListBySearchConditionForAdmin2() {
+        BoardSearchCondition condition = BoardSearchCondition.builder().email("tiger").build();
+        List<BoardResponse> boardResponses = boardService.getListBySearchConditionForAdmin(condition);
+        assertThat(boardResponses.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("ID & STATUS 조건 검색")
+    void getByIdAndStatus() {
+        BoardResponse boardResponse = boardService.getByIdAndStatus(1L, Status.ACTIVE);
+        assertThat(boardResponse.getTitle()).isEqualTo(BOARD_1_TITLE);
+    }
+
+    @Test
+    @DisplayName("조회수 증가")
+    void increaseHit() {
+        boardService.increaseHit(1L);
+        Board board = boardRepository.findById(1L).get();
+        assertThat(board.getHit()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("게시글 등록")
+    void register() {
+        BoardRequest boardRequest = BoardRequest.builder()
+            .title("hello")
+            .content("hi")
+            .build();
+        boardService.register(boardRequest);
+        assertThat(boardRepository.findById(4L).get()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("게시글 수정")
+    void edit() {
+        ModifyBoardRequest modifyRequest = ModifyBoardRequest.builder()
+            .title("hello")
+            .boardCategory(BoardCategory.FREE)
+            .build();
+
+        BoardResponse boardResponse = boardService.edit(1L, modifyRequest);
+
+        assertThat(boardResponse.getTitle()).isEqualTo("hello");
+        assertThat(boardResponse.getBoardCategory()).isEqualTo(BoardCategory.FREE);
+    }
+
+    @Test
+    @DisplayName("게시글 soft 삭제")
+    void deleteSoftly() {
+        boardService.deleteSoftly(2L);
+        Board board = boardRepository.findById(2L).get();
+
+        assertThat(board.getStatus()).isEqualTo(Status.INACTIVE);
+        assertThat(board.getDeletedDateTime()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("삭제된 게시글 복구")
+    void restore() {
+        boardService.deleteSoftly(2L);
+        boardService.restore(2L);
+
+        Board board = boardRepository.findById(2L).get();
+
+        assertThat(board.getStatus()).isEqualTo(Status.ACTIVE);
+        assertThat(board.getDeletedDateTime()).isNull();
+    }
+}
