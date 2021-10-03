a66d7181432451559f7787c843d5a85295d7f7ea
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2021-10-04 01:45:08 +0900

    [#92] Board domain Service & Controller 수정
    * [#92] Board domain Service & Controller 수정

--- module-service/src/main/java/com/woomoolmarket/service/board/BoardService.java
+++ module-service/src/main/java/com/woomoolmarket/service/board/BoardService.java
@@ -1,22 +1,23 @@
 package com.woomoolmarket.service.board;
 
-import static java.util.stream.Collectors.toList;
-
 import com.woomoolmarket.common.enumeration.Status;
 import com.woomoolmarket.common.util.ExceptionUtil;
 import com.woomoolmarket.domain.board.entity.Board;
 import com.woomoolmarket.domain.board.repository.BoardRepository;
+import com.woomoolmarket.domain.board.repository.BoardSearchCondition;
+import com.woomoolmarket.service.board.dto.request.BoardRequest;
 import com.woomoolmarket.service.board.dto.request.ModifyBoardRequest;
-import com.woomoolmarket.service.board.dto.request.RegisterBoardRequest;
 import com.woomoolmarket.service.board.dto.response.BoardResponse;
 import com.woomoolmarket.service.board.mapper.BoardResponseMapper;
 import com.woomoolmarket.service.board.mapper.ModifyBoardMapper;
 import com.woomoolmarket.service.board.mapper.RegisterBoardMapper;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,50 +32,17 @@ public class BoardService {
     private final RegisterBoardMapper registerBoardMapper;
     private final ModifyBoardMapper modifyBoardRequestMapper;
 
-    public List<BoardResponse> findAllBoards() {
-        return boardRepository.findAll()
+    @Cacheable(keyGenerator = "customKeyGenerator", value = "getListByCondition", unless = "#result==null")
+    public List<BoardResponse> getListBySearchCondition(BoardSearchCondition searchCondition) {
+        return boardRepository.findByCondition(searchCondition)
             .stream()
-            .parallel()
             .map(boardResponseMapper::toDto)
             .collect(Collectors.toList());
     }
 
-    public List<BoardResponse> findAllActiveBoards() {
-        return boardRepository.findAll()
-            .stream()
-            .parallel()
-            .filter(board -> board.getStatus() == Status.ACTIVE)
-            .map(boardResponseMapper::toDto)
-            .collect(Collectors.toList());
-    }
-
-    public List<BoardResponse> findAllInactiveBoards() {
-        return boardRepository.findAll()
-            .stream()
-            .parallel()
-            .filter(board -> board.getStatus() == Status.INACTIVE)
-            .map(boardResponseMapper::toDto)
-            .collect(Collectors.toList());
-    }
-
-    public BoardResponse findActiveBoard(Long id) {
-        return boardResponseMapper.toDto(boardRepository.findById(id)
+    public BoardResponse getByIdAndStatus(Long id, Status status) {
+        return boardResponseMapper.toDto(boardRepository.findByIdAndStatus(id, status)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionUtil.BOARD_NOT_FOUND)));
-    }
-
-//    public List<BoardResponse> findActiveBoarByNickname(String nickname) {
-//        return boardRepository.findAllByNickname(nickname)
-//                              .stream()
-//                              .map(boardResponseMapper::toDto)
-//                              .collect(Collectors.toList());
-//    }
-
-    public List<BoardResponse> findActiveBoards() {
-        return boardRepository.findAll()
-            .stream()
-            .filter(board -> board.getStatus() != Status.INACTIVE)
-            .map(boardResponseMapper::toDto)
-            .collect(toList());
     }
 
     @Transactional
@@ -85,22 +53,53 @@ public class BoardService {
     }
 
     @Transactional
-    public BoardResponse registerBoard(RegisterBoardRequest registerBoardRequest) {
-        return boardResponseMapper.toDto(boardRepository.save(registerBoardMapper.toEntity(registerBoardRequest)));
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchCondition", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListByConditionForAdmin", allEntries = true)})
+    public void register(BoardRequest boardRequest) {
+        Board board = registerBoardMapper.toEntity(boardRequest);
+        boardRepository.save(board);
     }
 
     @Transactional
-    public void editBoard(Long id, ModifyBoardRequest modifyBoardRequest) {
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchCondition", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListByConditionForAdmin", allEntries = true)})
+    public BoardResponse edit(Long id, ModifyBoardRequest modifyRequest) {
         Board board = boardRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionUtil.BOARD_NOT_FOUND));
 
-        modifyBoardRequestMapper.updateFromDto(modifyBoardRequest, board);
+        modifyBoardRequestMapper.updateFromDto(modifyRequest, board);
+        return boardResponseMapper.toDto(board);
     }
 
     @Transactional
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchCondition", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListByConditionForAdmin", allEntries = true)})
     public void deleteSoftly(Long id) {
         boardRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionUtil.BOARD_NOT_FOUND))
-            .changeStatus(Status.INACTIVE, LocalDateTime.now());
+            .delete();
+    }
+
+    @Transactional
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchCondition", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListByConditionForAdmin", allEntries = true)})
+    public void restore(Long id) {
+        boardRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(ExceptionUtil.BOARD_NOT_FOUND))
+            .restore();
+    }
+
+
+    /* FOR ADMIN */
+    @Cacheable(keyGenerator = "customKeyGenerator", value = "getListByConditionForAdmin", unless = "#result==null")
+    public List<BoardResponse> getListBySearchConditionForAdmin(BoardSearchCondition condition) {
+        return boardRepository.findByConditionForAdmin(condition)
+            .stream()
+            .map(boardResponseMapper::toDto)
+            .collect(Collectors.toList());
     }
 }
