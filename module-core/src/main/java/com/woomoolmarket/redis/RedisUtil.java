7fd5fd50b9b927a1a10c0b405756d02648e0790e
jm <rjm9303@gmail.com>
2021-10-11 16:10:20 +0900

    [#115] Change Redis Util class

--- module-core/src/main/java/com/woomoolmarket/redis/RedisUtil.java
+++ module-core/src/main/java/com/woomoolmarket/redis/RedisUtil.java
@@ -1,42 +1,43 @@
 package com.woomoolmarket.redis;
 
 import java.time.Duration;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class RedisUtil {
 
     private final StringRedisTemplate stringRedisTemplate;
+    private final ValueOperations<String, String> stringValueOperations;
+    private final HashOperations<String, String, String> hashValueOperations;
+
+    public RedisUtil(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.hashValueOperations = stringRedisTemplate.opsForHash();
+        this.stringValueOperations = stringRedisTemplate.opsForValue();
+    }
 
     public String getData(String key) {
-        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
-        return valueOperations.get(key);
+        return stringValueOperations.get(key);
     }
 
     public void setData(String key, Object value) {
-        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
-        valueOperations.set(key, String.valueOf(value));
+        stringValueOperations.set(key, String.valueOf(value));
     }
 
     public String getHashData(String key, String hashKey) {
-        HashOperations<String, Object, Object> valueOperations = stringRedisTemplate.opsForHash();
-        return (String) valueOperations.get(key, hashKey);
+        return hashValueOperations.get(key, hashKey);
     }
 
     public void setHashData(String key, String hashKey, Object value) {
-        HashOperations<String, String, String> valueOperations = stringRedisTemplate.opsForHash();
-        valueOperations.put(key, hashKey, String.valueOf(value));
+        hashValueOperations.put(key, hashKey, String.valueOf(value));
     }
 
     public void setDataExpire(String key, Object value, long duration) {
-        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
-        valueOperations.set(key, String.valueOf(value), expireDuration);
+        stringValueOperations.set(key, String.valueOf(value), expireDuration);
     }
 
     public void deleteData(String key) {
