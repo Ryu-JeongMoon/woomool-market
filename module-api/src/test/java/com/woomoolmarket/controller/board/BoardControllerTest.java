d17b971bb8bbd2800c60a4573339b35f72bcbdb0
jm <rjm9303@gmail.com>
2021-10-14 20:54:54 +0900

    [#131] Improve Restdocs Settings for Test

--- module-api/src/test/java/com/woomoolmarket/controller/board/BoardControllerTest.java
+++ module-api/src/test/java/com/woomoolmarket/controller/board/BoardControllerTest.java
@@ -1,60 +1,19 @@
 package com.woomoolmarket.controller.board;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.woomoolmarket.common.RestDocsConfiguration;
-import com.woomoolmarket.domain.board.repository.BoardRepository;
-import com.woomoolmarket.service.board.BoardService;
-import javax.persistence.EntityManager;
-import lombok.extern.log4j.Log4j2;
+import com.woomoolmarket.config.ApiDocumentationConfig;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.restdocs.JUnitRestDocumentation;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
+import org.junit.jupiter.api.Test;
 
-@Log4j2
-@Transactional
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@SpringBootTest
-class BoardControllerTest implements BeforeTestExecutionCallback {
 
-    @Autowired
-    MockMvc mockMvc;
-    @Autowired
-    WebApplicationContext context;
-    @Autowired
-    ObjectMapper objectMapper;
-    @Autowired
-    BoardRepository boardRepository;
-    @Autowired
-    EntityManager em;
-    @Autowired
-    BoardService boardService;
-
-    @Override
-    public void beforeTestExecution(ExtensionContext context) throws Exception {
-        JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-            .apply(documentationConfiguration(restDocumentation))
-            .build();
-    }
+class BoardControllerTest extends ApiDocumentationConfig {
 
     @BeforeEach
     void initialize() {
-        boardRepository.deleteAll();
         em.createNativeQuery("ALTER TABLE BOARD ALTER COLUMN `board_id` RESTART WITH 1").executeUpdate();
     }
 
+    @Test
+    void configTest() {
+        System.out.println("yaho");
+    }
 }
