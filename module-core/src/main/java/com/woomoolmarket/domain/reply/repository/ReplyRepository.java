1049fea64e00da2e3a9cdc0ca622669e6e713bee
jm <rjm9303@gmail.com>
2021-10-17 22:24:37 +0900

    [#140] Remove Reply Entity

--- module-core/src/main/java/com/woomoolmarket/domain/reply/repository/ReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.woomoolmarket.domain.reply.repository;
-
-import com.woomoolmarket.domain.reply.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
-}
