66781b0231274cd2287700ccd32b0c2ea91e2688
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2021-11-04 22:42:27 +0900

    [#181] Add check function for cool sms balance

--- module-service/src/main/java/com/woomoolmarket/service/auth/AuthFindService.java
+++ module-service/src/main/java/com/woomoolmarket/service/auth/AuthFindService.java
@@ -22,19 +22,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Log4j2
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class AuthFindService {
-
-    private final String EMAIL_TITLE = "Woomool-Market 임시 비밀번호 안내";
-    private final String EMAIL_CONTENT = "임시 비밀번호 - ";
-    private final String PHONE_CONTENT = "아이디 - ";
 
     private final Executor woomoolTaskExecutor;
     private final JavaMailSender javaMailSender;
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
-
     @Value("${mail.smtp.username}")
     private String WOOMOOL_MARKET_EMAIL;
     @Value("${coolsms.api-key}")
@@ -44,28 +38,25 @@ public class AuthFindService {
     @Value("${coolsms.phone-number}")
     private String WOOMOOL_MARKET_PHONE;
 
+    @Transactional
     public void sendAuthStringToEmail(String email) {
-        SecureRandom secureRandom = new SecureRandom();
-
         Member member = memberRepository.findByEmailAndStatus(email, Status.ACTIVE)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionUtil.MEMBER_NOT_FOUND));
 
+        SecureRandom secureRandom = new SecureRandom();
         int temporaryPassword = secureRandom.nextInt();
         member.changePassword(passwordEncoder.encode(String.valueOf(temporaryPassword)));
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setFrom(WOOMOOL_MARKET_EMAIL);
-        message.setSubject(EMAIL_TITLE);
-        message.setText(EMAIL_CONTENT + temporaryPassword);
+        message.setSubject("Woomool-Market 임시 비밀번호 안내");
+        message.setText("임시 비밀번호 - " + temporaryPassword);
 
-        CompletableFuture.runAsync(() -> javaMailSender.send(message), woomoolTaskExecutor)
-            .exceptionally(throwable -> {
-                log.error(throwable);
-                return null;
-            });
+        CompletableFuture.runAsync(() -> javaMailSender.send(message), woomoolTaskExecutor);
     }
 
+    @Transactional(readOnly = true)
     public void sendAuthStringToPhone(String phone) {
         Member member = memberRepository.findByPhoneAndStatus(phone, Status.ACTIVE)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionUtil.MEMBER_NOT_FOUND));
@@ -76,15 +67,31 @@ public class AuthFindService {
         smsParams.put("to", phone);
         smsParams.put("from", WOOMOOL_MARKET_PHONE);
         smsParams.put("type", "SMS");
-        smsParams.put("text", PHONE_CONTENT + member.getEmail());
+        smsParams.put("text", "아이디 - " + member.getEmail());
         smsParams.put("app_version", "woomool-market-v1.0");
 
-        CompletableFuture.runAsync(() -> {
-            try {
-                message.send(smsParams);
-            } catch (CoolsmsException e) {
-                log.error(e);
-            }
-        });
+        CompletableFuture.supplyAsync(this::checkBalance, woomoolTaskExecutor)
+            .thenAccept(balance -> {
+                if (balance < 20) {
+                    throw new RuntimeException(ExceptionUtil.NOT_ENOUGH_BALANCE);
+                }
+
+                try {
+                    message.send(smsParams);
+                } catch (CoolsmsException e) {
+                    log.error(e);
+                }
+            });
+    }
+
+    public int checkBalance() {
+        Message message = new Message(COOL_SMS_KEY, COOL_SMS_SECRET);
+
+        try {
+            return Integer.parseInt(message.balance().get("point").toString());
+        } catch (CoolsmsException e) {
+            log.error(e);
+            return 0;
+        }
     }
 }
