98f346a3935ded762a9f84a4af8b79124c6cd75a
jm <rjm9303@gmail.com>
2021-11-05 00:04:33 +0900

    [#182] Add AuditorAware Bean in JpaAuditingConfig

--- module-core/src/main/java/com/woomoolmarket/common/auditing/BaseEntity.java
+++ module-core/src/main/java/com/woomoolmarket/common/auditing/BaseEntity.java
@@ -1,11 +1,13 @@
 package com.woomoolmarket.common.auditing;
 
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 
 @Getter
+@MappedSuperclass
 public abstract class BaseEntity extends BaseTimeEntity {
 
     @CreatedBy
@@ -16,3 +18,14 @@ public abstract class BaseEntity extends BaseTimeEntity {
     protected String lastModifiedBy;
 
 }
+
+/*
+@EntityListeners(value = {AuditingEntityListener.class}) 속성은 상속되지만
+@MappedSuperclass 상속 안 되나봄
+
+두 애노테이션 다 정책은 아래와 같은데 뭐가 다르길래 안된걸까?!
+@Target({TYPE})
+@Retention(RUNTIME)
+
+설명을 조금 찾아보니 매핑 정보를 제공한다는 것을 명시적으로 제공해줘야하는 듯함
+ */
