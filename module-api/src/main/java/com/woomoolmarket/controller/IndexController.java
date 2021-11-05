0df4a22290d427425b6695145788b7b2bb5350f9
jm <rjm9303@gmail.com>
2021-11-05 17:29:41 +0900

    [#137] Add File Upload Function for S3 service

--- module-api/src/main/java/com/woomoolmarket/controller/IndexController.java
+++ module-api/src/main/java/com/woomoolmarket/controller/IndexController.java
@@ -1,14 +1,34 @@
 package com.woomoolmarket.controller;
 
+import com.woomoolmarket.service.s3.S3Uploader;
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Log4j2
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final S3Uploader s3Uploader;
+
+    @GetMapping("/upload")
+    public String uploadForm() {
+        return "file";
+    }
+
+    @ResponseBody
+    @PostMapping("/upload")
+    public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.upload(multipartFile, "static");
+    }
 
     @ResponseBody
     @GetMapping("/request")
