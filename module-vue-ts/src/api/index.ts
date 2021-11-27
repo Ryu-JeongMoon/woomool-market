007c6d23d0daaf135f6809a40e98c6196855562c
jm <rjm9303@gmail.com>
2021-11-28 03:20:59 +0900

    [#219] Add moving page logic in BoardListPage

--- module-vue-ts/src/api/index.ts
+++ module-vue-ts/src/api/index.ts
@@ -1,11 +1,7 @@
 import axios, { AxiosInstance } from "axios";
-import qs from "qs";
 import { setInterceptors } from "@/api/common/interceptors";
 
 const createInstance = (): AxiosInstance => {
-  axios.defaults.paramsSerializer = (params) => {
-    return qs.stringify(params);
-  };
   return axios.create({ baseURL: process.env.VUE_APP_LOCAL_URL });
 };
 
@@ -13,7 +9,3 @@ const createPrivateInstance = (): AxiosInstance => setInterceptors(publicAxios);
 
 export const publicAxios = createInstance();
 export const privateAxios = createPrivateInstance();
-
-// const instance = axios.create({
-//   baseURL: `${process.env.VUE_APP_LOCAL_URL}${url}`,
-// });
