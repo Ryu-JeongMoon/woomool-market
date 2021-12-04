a7a0f96b991251227ea8072dcf17bf478f2eb8c8
jm <rjm9303@gmail.com>
2021-12-05 04:46:55 +0900

    [#231] Refactor Service Layer with @Cacheable annotation - 3

--- module-service/src/main/java/com/woomoolmarket/service/product/ProductService.java
+++ module-service/src/main/java/com/woomoolmarket/service/product/ProductService.java
@@ -16,12 +16,14 @@ import com.woomoolmarket.service.product.mapper.ProductResponseMapper;
 import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -29,13 +31,15 @@ public class ProductService {
     private final ProductResponseMapper productResponseMapper;
     private final ProductModifyRequestMapper productModifyRequestMapper;
 
+    @Transactional(readOnly = true)
     public ProductResponse getByIdAndStatus(Long id, Status status) {
         return productRepository.findByIdAndStatus(id, status)
             .map(productResponseMapper::toDto)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionConstant.PRODUCT_NOT_FOUND));
     }
 
-    //@Cacheable(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForMember", unless = "#result==null")
+    @Transactional(readOnly = true)
+    @Cacheable(keyGenerator = "customKeyGenerator", value = "products", unless = "#result == null", cacheManager = "cacheManager")
     public List<ProductResponse> getListBySearchConditionForMember(ProductSearchCondition searchCondition) {
         return productRepository.findByCondition(searchCondition)
             .stream()
@@ -44,18 +48,18 @@ public class ProductService {
     }
 
     @Transactional
-//    @Caching(evict = {
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForMember", allEntries = true),
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForAdmin", allEntries = true)})
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "products", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "productsForAdmin", allEntries = true)})
     public void create(ProductRequest productRequest) {
         Product product = productRequestMapper.toEntity(productRequest);
         productRepository.save(product);
     }
 
     @Transactional
-//    @Caching(evict = {
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForMember", allEntries = true),
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForAdmin", allEntries = true)})
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "products", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "productsForAdmin", allEntries = true)})
     public ProductResponse edit(Long id, ProductModifyRequest modifyRequest) {
         Product product = productRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionConstant.PRODUCT_NOT_FOUND));
@@ -64,9 +68,9 @@ public class ProductService {
     }
 
     @Transactional
-//    @Caching(evict = {
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForMember", allEntries = true),
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForAdmin", allEntries = true)})
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "products", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "productsForAdmin", allEntries = true)})
     public void deleteSoftly(Long id) {
         productRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionConstant.PRODUCT_NOT_FOUND))
@@ -74,9 +78,9 @@ public class ProductService {
     }
 
     @Transactional
-//    @Caching(evict = {
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForMember", allEntries = true),
-//        @CacheEvict(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForAdmin", allEntries = true)})
+    @Caching(evict = {
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "products", allEntries = true),
+        @CacheEvict(keyGenerator = "customKeyGenerator", value = "productsForAdmin", allEntries = true)})
     public void restore(Long id) {
         productRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException(ExceptionConstant.PRODUCT_NOT_FOUND))
@@ -84,7 +88,8 @@ public class ProductService {
     }
 
     /* FOR ADMIN */
-//    @Cacheable(keyGenerator = "customKeyGenerator", value = "getListBySearchConditionForAdmin", unless = "#result==null")
+    @Transactional(readOnly = true)
+    @Cacheable(keyGenerator = "customKeyGenerator", value = "productsForAdmin", unless = "#result == null", cacheManager = "cacheManager")
     public List<ProductResponse> getListBySearchConditionForAdmin(ProductSearchCondition searchCondition) {
         return productRepository.findByConditionForAdmin(searchCondition)
             .stream()
@@ -92,4 +97,3 @@ public class ProductService {
             .collect(toList());
     }
 }
-// TODO, Product CacheManager 할당 필요
