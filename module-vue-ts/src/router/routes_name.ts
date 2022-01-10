8259c32f393b61eacae81ffc66a944fdab41b31f
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2022-01-10 20:29:22 +0900

    [#327] Create ROUTES_NAME for routes.ts

---
+++ module-vue-ts/src/router/routes_name.ts
@@ -0,0 +1,45 @@
+export const ROUTES_NAME = {
+  ROOT: "Root",
+  ABOUT: "About",
+
+  AUTH: {
+    LOGIN: "Login",
+    OAUTH2: "OAuth2Login",
+    SIGNUP: "Signup",
+  },
+
+  MEMBER: {
+    LIST: "Members",
+    DETAIL: "DetailMembers",
+    MODIFY: "ModifyMembers",
+  },
+
+  BOARD: {
+    LIST: "Boards",
+    CREATE: "CreateBoards",
+    DETAIL: "DetailBoards",
+    MODIFY: "ModifyBoards",
+  },
+
+  PRODUCT: {
+    LIST: "Products",
+    CREATE: "CreateProducts",
+    DETAIL: "DetailProducts",
+    MODIFY: "ModifyProducts",
+  },
+
+  CART: {
+    CREATE: "CreateCarts",
+    DETAIL: "Carts",
+    ADMIN: "CartsForAdmin",
+  },
+
+  ORDER: {
+    LIST: "Orders",
+    CREATE: "CreateOrders",
+    DETAIL: "DetailOrders",
+    MODIFY: "ModifyOrders",
+  },
+
+  BACK: "/back",
+};
