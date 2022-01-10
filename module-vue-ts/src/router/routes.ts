28f77a5da015033d67a80f23383a4753ca14cc06
jm <rjm9303@gmail.com>
2022-01-10 20:23:58 +0900

    [#327] Create ROUTES_NAME for routes.ts

--- module-vue-ts/src/router/routes.ts
+++ module-vue-ts/src/router/routes.ts
@@ -1,48 +1,46 @@
 import { RouteConfig } from "vue-router";
 import { PATH } from "@/router/routes_path";
-import { Pageable } from "@/interfaces/common/page";
+import { ROUTES_NAME } from "@/router/routes_name";
 
 const routes: Array<RouteConfig> = [
   {
+    name: ROUTES_NAME.ROOT,
     path: PATH.ROOT,
-    name: "Home",
   },
   {
+    name: ROUTES_NAME.ABOUT,
     path: PATH.ABOUT,
-    name: "About",
     component: () => import("@/views/About.vue"),
   },
   {
+    name: ROUTES_NAME.AUTH.LOGIN,
     path: PATH.AUTH.LOGIN,
-    name: "Login",
     component: () => import("@/views/auth/LoginPage.vue"),
   },
   {
+    name: ROUTES_NAME.AUTH.OAUTH2,
     path: PATH.AUTH.OAUTH2,
-    name: "OAuth2Login",
     component: () => import("@/views/member/OAuth2LoginPage.vue"),
   },
   {
-    name: "Signup",
+    name: ROUTES_NAME.AUTH.SIGNUP,
     path: PATH.AUTH.SIGNUP,
     component: () => import("@/views/member/SignupPage.vue"),
   },
 
   {
-    name: "Members",
+    name: ROUTES_NAME.MEMBER.LIST,
     path: PATH.MEMBER.LIST,
     component: () => import("@/views/member/MemberListPage.vue"),
   },
   {
-    name: "DetailMembers",
+    name: ROUTES_NAME.MEMBER.DETAIL,
     path: PATH.MEMBER.DETAIL,
+    props: true,
     component: () => import("@/views/member/MemberDetailPage.vue"),
-    props: (route) => ({
-      memberId: route.params.memberId,
-    }),
   },
   {
-    name: "ModifyMembers",
+    name: ROUTES_NAME.MEMBER.MODIFY,
     path: PATH.MEMBER.MODIFY,
     component: () => import("@/views/member/MemberModifyPage.vue"),
     props: (route) => ({
@@ -51,7 +49,7 @@ const routes: Array<RouteConfig> = [
   },
 
   {
-    name: "Boards",
+    name: ROUTES_NAME.BOARD.LIST,
     path: PATH.BOARD.LIST,
     component: () => import("@/views/board/BoardListPage.vue"),
     props: (route) => ({
@@ -60,12 +58,12 @@ const routes: Array<RouteConfig> = [
     }),
   },
   {
-    name: "CreateBoards",
+    name: ROUTES_NAME.BOARD.CREATE,
     path: PATH.BOARD.CREATE,
     component: () => import("@/views/board/BoardCreatePage.vue"),
   },
   {
-    name: "DetailBoards",
+    name: ROUTES_NAME.BOARD.DETAIL,
     path: PATH.BOARD.DETAIL,
     component: () => import("@/views/board/BoardDetailPage.vue"),
     props: (route) => ({
@@ -73,13 +71,13 @@ const routes: Array<RouteConfig> = [
     }),
   },
   {
-    name: "ModifyBoards",
+    name: ROUTES_NAME.BOARD.MODIFY,
     path: PATH.BOARD.MODIFY,
     component: () => import("@/views/board/BoardModifyPage.vue"),
   },
 
   {
-    name: "Carts",
+    name: ROUTES_NAME.CART.DETAIL,
     path: PATH.CART.DETAIL,
     component: () => import("@/views/cart/CartDetailPage.vue"),
     props: (route) => ({
@@ -88,57 +86,57 @@ const routes: Array<RouteConfig> = [
     }),
   },
   {
-    name: "CreateCarts",
+    name: ROUTES_NAME.CART.CREATE,
     path: PATH.CART.CREATE,
     component: () => import("@/views/cart/CartCreatePage.vue"),
   },
   {
-    name: "CartsForAdmin",
+    name: ROUTES_NAME.CART.ADMIN,
     path: PATH.CART.ADMIN,
+    props: true,
     component: () => import("@/views/cart/CartListPage.vue"),
-    props: (route) => ({
-      pageable: route.params.pageable,
-    }),
   },
 
   {
-    name: "Products",
+    name: ROUTES_NAME.PRODUCT.LIST,
     path: PATH.PRODUCT.LIST,
     component: () => import("@/views/product/ProductListPage.vue"),
   },
   {
-    name: "CreateProducts",
+    name: ROUTES_NAME.PRODUCT.CREATE,
     path: PATH.PRODUCT.CREATE,
     component: () => import("@/views/product/ProductCreatePage.vue"),
   },
   {
-    name: "DetailProducts",
+    name: ROUTES_NAME.PRODUCT.DETAIL,
     path: PATH.PRODUCT.DETAIL,
+    props: true,
     component: () => import("@/views/product/ProductDetailPage.vue"),
   },
   {
-    name: "ModifyProducts",
+    name: ROUTES_NAME.PRODUCT.MODIFY,
     path: PATH.PRODUCT.MODIFY,
     component: () => import("@/views/product/ProductModifyPage.vue"),
   },
 
   {
-    name: "Orders",
+    name: ROUTES_NAME.ORDER.LIST,
     path: PATH.ORDER.LIST,
     component: () => import("@/views/order/OrderListPage.vue"),
   },
   {
-    name: "CreateOrders",
+    name: ROUTES_NAME.ORDER.CREATE,
     path: PATH.ORDER.CREATE,
     component: () => import("@/views/order/OrderCreatePage.vue"),
+    props: true,
   },
   {
-    name: "DetailOrders",
+    name: ROUTES_NAME.ORDER.DETAIL,
     path: PATH.ORDER.DETAIL,
     component: () => import("@/views/order/OrderDetailPage.vue"),
   },
   {
-    name: "ModifyOrders",
+    name: ROUTES_NAME.ORDER.MODIFY,
     path: PATH.ORDER.MODIFY,
     component: () => import("@/views/order/OrderModifyPage.vue"),
   },
