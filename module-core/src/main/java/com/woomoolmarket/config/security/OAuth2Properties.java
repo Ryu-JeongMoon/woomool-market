9c27854c1d48894acc5924baba5a095b4f287b32
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2022-03-29 00:26:33 +0900

    refactor($oauth2-config): change properties binding (#346)
    - add enum-utils
    - add @ConfigurationProperties instead of env.getProperty

---
+++ module-core/src/main/java/com/woomoolmarket/config/security/OAuth2Properties.java
@@ -0,0 +1,34 @@
+package com.woomoolmarket.config.security;
+
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "spring.security.oauth2.client")
+public class OAuth2Properties {
+
+  private final Map<Client, Resource> registration;
+
+  enum Client {
+    NAVER,
+    KAKAO,
+    GOOGLE,
+    GITHUB,
+    FACEBOOK
+  }
+
+  @Getter
+  @ConstructorBinding
+  @RequiredArgsConstructor
+  static class Resource {
+
+    private final String clientName;
+    private final String clientId;
+    private final String clientSecret;
+  }
+}
