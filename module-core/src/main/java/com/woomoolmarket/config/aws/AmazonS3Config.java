344f82381da64f9813129189a3b27a52020523fc
jm <rjm9303@gmail.com>
2022-03-29 01:10:38 +0900

    refactor($aws-s3-config): change properties binding
    - add @ConfigurationProperties instead of @Value

--- module-core/src/main/java/com/woomoolmarket/config/aws/AmazonS3Config.java
+++ module-core/src/main/java/com/woomoolmarket/config/aws/AmazonS3Config.java
@@ -4,27 +4,27 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(AwsS3Properties.class)
 public class AmazonS3Config {
 
-  @Value("${cloud.aws.credentials.accessKey}")
-  private String accessKey;
-
-  @Value("${cloud.aws.credentials.secretKey}")
-  private String secretKey;
-
-  @Value("${cloud.aws.region.static}")
-  private String region;
+  private final AwsS3Properties awsS3Properties;
 
   @Bean
   public AmazonS3Client amazonS3Client() {
-    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
+      awsS3Properties.getCredentials().getAccessKey(),
+      awsS3Properties.getCredentials().getSecretKey()
+    );
+
     return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-      .withRegion(region)
+      .withRegion(awsS3Properties.getRegion().getStatic())
       .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
       .build();
   }
--- module-core/src/test/java/com/woomoolmarket/config/aws/AmazonS3ConfigTest.java
+++ module-core/src/test/java/com/woomoolmarket/config/aws/AmazonS3ConfigTest.java
@@ -1,25 +1,41 @@
 package com.woomoolmarket.config.aws;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import lombok.extern.log4j.Log4j2;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.woomoolmarket.config.aws.AwsS3Properties.Credentials;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 
-@Log4j2
 @SpringBootTest
 class AmazonS3ConfigTest {
 
   @Autowired
-  Environment env;
+  AmazonS3Client amazonS3Client;
+  @Autowired
+  AwsS3Properties awsS3Properties;
 
   @Test
-  @DisplayName("yml 에서 올바른 정보 읽어온다")
-  void configureTest() {
-    String region = env.getProperty("cloud.aws.region.static");
-    assertThat(region).isEqualTo("ap-northeast-2");
+  @DisplayName("properties binding 성공")
+  void awsS3Properties() {
+    String region = awsS3Properties.getRegion().getStatic();
+    Credentials credentials = awsS3Properties.getCredentials();
+
+    assertAll(
+      () -> assertThat(region).isEqualTo("ap-northeast-2"),
+      () -> assertThat(credentials.getAccessKey()).isNotBlank(),
+      () -> assertThat(credentials.getSecretKey()).isNotBlank()
+    );
+
+  }
+
+  @Test
+  @DisplayName("AmazonS3Client 설정 성공")
+  void amazonS3Client() {
+    assertNotNull(amazonS3Client);
   }
 }
