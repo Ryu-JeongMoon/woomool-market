448f65f041883970e91accba03b568b94d0e4fb6
jm <rjm9303@gmail.com>
2022-03-29 00:18:20 +0900

    refactor($oauth2-config): change properties binding
    - add enum-utils
    - add @ConfigurationProperties instead of env.getProperty

--- module-core/src/main/java/com/woomoolmarket/config/security/OAuth2SecurityConfig.java
+++ module-core/src/main/java/com/woomoolmarket/config/security/OAuth2SecurityConfig.java
@@ -4,39 +4,40 @@ import com.woomoolmarket.security.oauth2.CustomOAuth2Provider;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.EnumUtils;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(OAuth2Properties.class)
 public class OAuth2SecurityConfig {
 
-  private final static List<String> CLIENTS = List.of("google", "facebook", "naver", "kakao", "github");
-  private final static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
-
-  private final Environment env;
+  private final OAuth2Properties oAuth2Properties;
 
   @Bean
   public ClientRegistrationRepository clientRegistrationRepository() {
-    List<ClientRegistration> registrations = CLIENTS.stream()
+    List<ClientRegistration> registrations = oAuth2Properties.getRegistration()
+      .keySet()
+      .stream()
       .map(this::getRegistration)
       .collect(Collectors.toList());
 
     return new InMemoryClientRegistrationRepository(registrations);
   }
 
-  private ClientRegistration getRegistration(String client) {
-    String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
-    String clientSecret = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-secret");
+  private ClientRegistration getRegistration(OAuth2Properties.Client client) {
+    OAuth2Properties.Resource resource = oAuth2Properties.getRegistration().get(client);
+    String clientName = resource.getClientName();
 
-    return CustomOAuth2Provider.valueOf(client.toUpperCase())
-      .getBuilder(client)
-      .clientId(clientId)
-      .clientSecret(clientSecret)
+    return EnumUtils.getEnumIgnoreCase(CustomOAuth2Provider.class, clientName)
+      .getBuilder(clientName)
+      .clientId(resource.getClientId())
+      .clientSecret(resource.getClientSecret())
       .build();
   }
 }
